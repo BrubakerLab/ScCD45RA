637caf3ffc5665c050abbc272c928bbe704bed93
Ran-x2 <112669569+Ran-x2@users.noreply.github.com>
2023-07-24 01:23:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="sccd45ra",
-    version="0.1",
+    version="0.3.2",
     packages=find_packages(),
-    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
     package_data={
         'sccd45ra': ['model/*.h5ad', 'model/*.joblib','feature/*.xlsx'],
     },
+    install_requires=[
+        "scanpy",
+        "numpy",
+        "pandas",
+        "anndata>=0.8.0",
+        "joblib",
+        "scikit-learn",
+        "setuptools"
+    ]
 )
